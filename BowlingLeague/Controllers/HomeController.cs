bd5448185ec3dbe26468990e89a79640479f0e61
kdaniel1 <kenn97@byu.edu>
2021-04-05 20:53:07 -0600

    Add project files.

---
+++ BowlingLeague/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using BowlingLeague.Models;
+using BowlingLeague.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BowlingLeague.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BowlingLeagueContext context { get; set; }
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger, BowlingLeagueContext ctx)
+        {
+            _logger = logger;
+            context = ctx;
+        }
+
+        public IActionResult Index(long? teamId, string teamName, int pageNum = 0)
+        {
+            int pageSize = 5;
+            return View(new IndexViewModel
+            {
+                Bowlers = context.Bowlers
+                    .Where(b => b.TeamId == teamId || teamId == null)
+                    .OrderBy(b => b.Team)
+                    .Skip((pageNum - 1) * pageSize)
+                    .Take(pageSize)
+                    .ToList(),
+                PageNumberingInfo = new PageNumberingInfo
+                {
+                    NumItemsPerPage = pageSize,
+                    CurrentPage = pageNum,
+                    TotalNumItems = (teamId == null ? context.Bowlers.Count() :
+                    context.Bowlers.Where(b => b.TeamId == teamId).Count())
+                },
+                TeamName = teamName
+            });
+        }
+            public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BowlingLeague/Infrastructure/PaginationTagHelper.cs
@@ -0,0 +1,63 @@
+﻿using BowlingLeague.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BowlingLeague.Infrastructure
+{
+    /* setting an attribtue allows us to use that within the div tag 
+     * to set the attribute equal to something*/
+    [HtmlTargetElement("div", Attributes = "page-info")]
+    public class PaginationTagHelper : TagHelper
+    {
+        private IUrlHelperFactory urlInfo;
+        public PaginationTagHelper(IUrlHelperFactory uhf)
+        {
+            urlInfo = uhf;
+        }
+        //use this attribute (in the html target element to set)
+        public PageNumberingInfo PageInfo { get; set; }
+        // for dynamically changing the styling:
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set;}
+
+        public string TeamName { get; set; }
+        //create a dictionary w/ key value pairs makes it easier to pass parameters
+        [HtmlAttributeName(DictionaryAttributePrefix = "page-url-")]
+        public Dictionary<string, object> KeyValuePairs { get; set; } = new Dictionary<string, object>();
+        [HtmlAttributeNotBound]
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            IUrlHelper urlHelp = urlInfo.GetUrlHelper(ViewContext);
+            TagBuilder finishedTag = new TagBuilder("div");
+
+            for (int i = 1; i <= PageInfo.NumPages; i++)
+            {
+                TagBuilder individualTag = new TagBuilder("a");
+                KeyValuePairs["pageNum"] = i;
+
+                individualTag.Attributes["href"] = urlHelp.Action("Index", KeyValuePairs);
+                individualTag.InnerHtml.AppendHtml(i.ToString());
+
+                if (PageClassesEnabled)
+                {
+                    finishedTag.AddCssClass(PageClass);
+                    finishedTag.AddCssClass(i == PageInfo.CurrentPage ? PageClassSelected : PageClassNormal);
+                }
+
+                finishedTag.InnerHtml.AppendHtml(individualTag);
+            }
+            output.Content.AppendHtml(finishedTag.InnerHtml);
+        }
+    }
+}
